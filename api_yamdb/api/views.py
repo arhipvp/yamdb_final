f675190a6d765f86fea50cce5bbcf535c886f4b4
Архипов Владимир <arhipvp@gmail.com>
2023-05-12 21:53:05 +0300

    2

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -9,9 +9,10 @@ from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework_simplejwt.tokens import RefreshToken
+from reviews.models import Category, Genre, Review, Title, User
 
 from api_yamdb.settings import ADMIN_EMAIL
-from reviews.models import Category, Genre, Review, Title, User
+
 from .filters import TitleFilter
 from .permissions import (IsAdminOrReadOnly, IsAdminOrSuperUser,
                           IsAuthorOrModeratorOrAdminOrSuperuser)
